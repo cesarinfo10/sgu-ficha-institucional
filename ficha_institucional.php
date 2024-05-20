321b40e3aecb08c14f8a798d44a964ca92a4bcae
casv <cesarinfo10@gmail.com>
2024-05-20 20:07:08 -0300

    Información Consolidada 0.1

--- ficha_institucional.php
+++ ficha_institucional.php
@@ -1,11 +1,141 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <h1>Ficha Institucional</h1>
+
+<div class="container mt-3">
+  <h3>Ficha Institucional</h3>
+  <br>
+  <!-- Nav tabs -->
+  <ul class="nav nav-tabs">
+    <li class="nav-item">
+      <a class="nav-link active" data-toggle="tab" href="#home">Información Consolidada</a>
+    </li>
+    <li class="nav-item">   
+      <a class="nav-link" data-toggle="tab" href="#menu1">Ofera de Pregrado</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="tab" href="#menu2">Matrícula de Pregrado</a>
+    </li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div id="home" class="container tab-pane active"><br>
+      <h3>Información Consolidada</h3>
+    <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th></th>
+      <th class="tituloTabla">Proceso anterior [1]</th>
+      <th class="tituloTabla">Proceso actual (año actual)</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td class="text-left tituloTabla">N° de sedes</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">N° de carreras de pregrado (especificar niveles) [2]</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">Áreas del conocimiento [3]</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">Matrícula nueva pregrado</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">Matrícula nueva pregrado (% Mujer) [4]</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">Matricula total pregrado</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">Matricula total pregrado (% Mujer)</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">N° académicos</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">N° académicos (% Mujer)</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">JCE [5]</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">JCE (% Mujer)</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">N° programas magister</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">N° programas doctorados</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">Matrícula nueva postgrados</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">Matrícula nueva postgrados (% Mujer)</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">Matricula total postgrados</td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="text-left tituloTabla">Matricula total postgrados (% Mujer)</td>
+      <td></td>
+      <td></td>
+    </tr>
+    </tbody>
+</table>
+    </div>
+    <div id="menu1" class="container tab-pane fade"><br>
+      <h3>Menu 1</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="menu2" class="container tab-pane fade"><br>
+      <h3>Menu 2</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+  </div>
+</div>
+
 </body>
 </html>
